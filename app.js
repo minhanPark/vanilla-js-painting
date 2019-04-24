f2cafad060e5fa4b55a05b223a8a0caf6a7f3bc5
minhan <alsgks1218@gmail.com>
2019-04-24 23:18:44 +0900

    add change color func

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#34495e";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting() {
+  painting = true;
+}
 
 function stopPainting() {
   painting = false;
@@ -9,19 +21,28 @@ function stopPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
